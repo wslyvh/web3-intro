ebfd307955929ed48c5481ca34272a4efd5e86a6
wslyvh <wslyvh@users.noreply.github.com>
2021-06-18 15:33:11 +0200

    rinkeby deployer

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,4 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+// Be aware of NEVER sharing or committing your secret seed, or private key with others
+// Be aware of NEVER putting real Ether into testing accounts
+// Your secret seed or private key should be kept private at all times! 
+const RINKEBY_DEPLOYER_PRIVATE_KEY = process.env.RINKEBY_DEPLOYER_PRIVATE_KEY
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -12,11 +18,15 @@ task("accounts", "Prints the list of accounts", async () => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: { },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/" + process.env.INFURA_ID,
+      accounts: [RINKEBY_DEPLOYER_PRIVATE_KEY]
+    }
+  }
 };
 
